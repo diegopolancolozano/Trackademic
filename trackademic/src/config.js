a02e1235e2bdf0b75e873490b10fd9e67f10d451
Martin Gomez <agazazaca@gmail.com>
2025-05-18 16:44:38 -0500

    feat: vite + react template and base connections

---
+++ trackademic/src/config.js
@@ -0,0 +1,13 @@
+export const config = {
+    supabaseTables: {
+      students: "students",
+      courses: "courses",
+      grades: "grades",
+      semesters: "semesters"
+    },
+    mongoCollections: {
+      evaluations: "evaluations",
+      comments: "comments"
+    }
+  };
+  
